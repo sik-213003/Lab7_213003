e87b1d86117e4768ba6bb5149c71d381ed84c932
sik-213003 <145955613+sik-213003@users.noreply.github.com>
2023-11-13 11:51:53 +0500

    Add files via upload

---
+++ Q5/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int fact = 1;
+            int number = int.Parse(textBox1.Text);
+
+            for (int i = 1; i <= number; i++)
+            {
+                fact = fact * i;
+            }
+
+            textBox2.Text = fact.ToString();
+
+        }
+    }
+}
